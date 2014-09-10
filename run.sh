5a8b48f8be581860bb04825a6e49309ad0c6f0f3
Saulo Alves <sauloal@gmail.com>
2014-09-11 00:04:24 +0200

    right data folder

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker stop node; docker rm node
 
 SRC=$HOME/data
-SRC_DATA=$SRC/node
+SRC_DATA=$SRC/docker/node
 
 docker run -d --name node -v $SRC_DATA:/data -p 127.0.1.1:8050:8080 dockerfile/nodejs bash /data/run.sh
